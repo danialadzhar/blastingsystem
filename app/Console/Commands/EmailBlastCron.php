35fd1cb5906121070cadddd44afff57d74e27906
Mohd Danial Bin Mohd Adzhar <10921402+danialadzhar@users.noreply.github.com>
2021-12-06 14:49:09 +0800

    Create EmailBlastCron.php

---
+++ app/Console/Commands/EmailBlastCron.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\EmailCron;
+
+class EmailBlastCron extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'blast:email';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Blast Email with Cron Job';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        // return Command::SUCCESS;
+
+        $email_cron = EmailCron::first();
+
+        $details = [
+            'subject' => $email_cron->subject,
+            'email_content' => $email_cron->email_content,
+            'group_id' => $email_cron->group_id,
+            'email_from' => $email_cron->email_from,
+            'name_from' => $email_cron->name_from,
+        ];
+
+        $job = (new \App\Jobs\SendQueueEmail($details))->delay(now()->addSeconds(2));
+
+        dispatch($job);
+
+    }
+}
